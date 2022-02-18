0065e09efa4002b6572b34f717326377a9ff6e22
FUrryUp <hirrochi0211@icloud.com>
2022-02-18 23:52:16 +0900

    finish the detail.html, markdown html, inclusion tag.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.html import strip_tags
 from django.urls import reverse
+import markdown
 
 # Create your models here.
 
@@ -35,10 +37,20 @@ class Post(models.Model):
     # author <= default
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # Reuse the save method to update the created_time field each time
-    # def save(self, *args, **kwargs):
-    #     self.modified_time = timezone.now()
-    #     super().save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        # Reuse the save method to update the created_time field each time
+        self.modified_time = timezone.now()
+
+        # Reuse the save method to create an excerpt text automatically
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
+        # strip_tags can remove all of the html tags.
+        self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post
+import markdown
+import re
 
 
 # Create your views here.
@@ -14,4 +16,21 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # if obj of Post in database has the pk, it will return the object, else, 404
-    return render(request, 'blog/detail.html', context={'post':post})
+
+    # post.body = markdown.markdown(post.body,
+    #                               extensions=[
+    #                                   'markdown.extensions.extra',
+    #                                   'markdown.extensions.codehilite',
+    #                                   'markdown.extensions.toc',
+    #                               ])
+    md = markdown.Markdown(extensions=[
+                            'markdown.extensions.extra',
+                            'markdown.extensions.codehilite',
+                            'markdown.extensions.toc',
+    ])
+    post.body = md.convert(post.body)
+    # if post.body do not have any contents, post.toc is not necessary to be displayed.
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+    return render(request, 'blog/detail.html', context={'post': post})
